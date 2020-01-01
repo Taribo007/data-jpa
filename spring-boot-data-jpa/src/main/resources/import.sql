70b3de0f4dd079a7e378ae5ff27127d8e7ca45d4
Andrés González <Andrs Gonzlez@DESKTOP-S7BCS61>
2020-01-01 22:11:36 +0100

    Configurando para mySql

--- spring-boot-data-jpa/src/main/resources/import.sql
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -1,2 +1,2 @@
 INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (1,'Andres','Gonzalez','mail1@gamil.com','2019-08-28');
-INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (2,'Olga','Rodriguez','mail2@gamil.com','2018-08-18');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (2,'Olga','Rodriguez','mailol@gamil.com','2018-08-18');
