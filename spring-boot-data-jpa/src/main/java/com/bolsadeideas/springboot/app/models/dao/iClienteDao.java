13eda608e1307dda0100f985daf2310f9e6ce209
Andrés González <Andrs Gonzlez@DESKTOP-S7BCS61>
2020-02-20 19:14:42 +0100

    paginador parte 1

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/iClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/iClienteDao.java
@@ -1,11 +1,11 @@
 package com.bolsadeideas.springboot.app.models.dao;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
-public interface iClienteDao extends CrudRepository<Cliente,Long>{
+public interface iClienteDao extends PagingAndSortingRepository<Cliente,Long>{
 
 	
 }
